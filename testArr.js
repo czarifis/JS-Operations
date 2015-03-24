4dac4ad8be06cbb17e5451507b6ed8e8406e05fc
xhrdx <zarifis@eng.ucsd.edu>
2015-03-24 00:33:27 -0700

    array tests

--- testArr.js
+++ testArr.js
@@ -70,6 +70,7 @@ function timePopOfArray(size){
     var end = new Date().getTime();
     var time = end - start;
     console.log('Generating an array takes: ' + time);
+    return array10k
 }
 
 function add2Array() {
@@ -98,3 +99,96 @@ array25 = populateArray(25);
 array100 = populateArray(100);
 ll25 = populateLinkedList(25);
 ll100 = populateLinkedList(100);
+
+
+
+function timedAdd2CustArray(Arr){
+    var start = new Date().getTime();
+    Arr.splice(1, 0, "new item");
+    var end = new Date().getTime();
+    var time = end - start;
+    console.log('Adding an element into the array takes: ' + time);
+}
+
+var array1k;
+function populate1K(){
+    array1k = timePopOfArray(1000)
+}
+
+function addTo1KArray(){
+    timedAdd2CustArray(array1k)
+}
+function addToMiddleOf1KArray(){
+    timedAdd2MiddleOfCustArray(array1k,1000)
+}
+
+
+var array10000;
+function populate10K(){
+    array10000 = timePopOfArray(10000)
+}
+
+function addTo10KArray(){
+    timedAdd2CustArray(array10000)
+}
+function addToMiddleOf10KArray(){
+    timedAdd2MiddleOfCustArray(array10000, 10000)
+}
+
+var array1M;
+function populate1M(){
+    array1M = timePopOfArray(1000000)
+}
+
+function addTo1MArray(){
+    timedAdd2CustArray(array1M)
+}
+
+function addToMiddleOf1MArray(){
+    timedAdd2MiddleOfCustArray(array1M)
+}
+
+var array10M;
+function populate10M(){
+    array10M = timePopOfArray(10000000)
+}
+
+function addTo10MArray(){
+    timedAdd2CustArray(array10M)
+}
+
+function addToMiddleOf10MArray(){
+    timedAdd2MiddleOfCustArray(array10M)
+}
+
+var array100M;
+function populate100M(){
+    array100M = timePopOfArray(100000000)
+}
+
+function addTo100MArray(){
+    timedAdd2CustArray(array100M)
+}
+function addToMiddleOf100MArray(){
+    timedAdd2MiddleOfCustArray(array100M)
+}
+
+var array1T;
+function populate1T(){
+    array1T = timePopOfArray(1000000000)
+}
+
+function addTo1TArray(){
+    timedAdd2CustArray(array1T)
+}
+function addToMiddleOf1T(){
+    timedAdd2MiddleOfCustArray(array100M)
+}
+
+function timedAdd2MiddleOfCustArray(Arr){
+    var start = new Date().getTime();
+    Arr.splice(Arr.length/2, 0, "new item");
+    var end = new Date().getTime();
+    var time = end - start;
+    console.log('Adding an element into the middle of the array takes: ' + time);
+}
